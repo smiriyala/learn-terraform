fa942eab6bc7c7bb0fd8a93e6d73e69e17bca9a9
subbum <subbu@gmail.com>
2023-04-28 21:48:11 +0100

    ami id fetching dynamically

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "ec2" {
-    ami = "ami-0089b8e98cd95257d"
+    # ami = "ami-0089b8e98cd95257d"
+    ami = "${data.aws_ami.latest-ubuntu.id}"
     instance_type = "t3.micro"
     vpc_security_group_ids = [ "sg-0111cc490d6b33509" ]
     tags = {
