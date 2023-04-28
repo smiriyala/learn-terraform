b79c9fb4d46789f48bfba179c7681c320283117b
subbum <subbu@gmail.com>
2023-04-28 21:36:05 +0100

    updated

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0089b8e98cd95257d"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0111cc490d6b33509"]
+    vpc_security_group_ids = [ "sg-0111cc490d6b33509" ]
     tags = {
       Name = "test"
     }
+    
 
   
 }
