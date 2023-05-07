8fde8fcd2d67cbfcc3d38548d38fd57bbc31737e
subbum <subbu@gmail.com>
2023-05-07 08:14:15 +0100

    loops with variables

---
+++ 06-loops/main.tf
@@ -0,0 +1,48 @@
+
+##==============Varibles==============
+variable "d1" {
+    default = 5
+
+}
+
+#access single variable in resource block
+resource "null_resource" "NoResource" {
+
+    count = var.d1
+  
+}
+
+##==============List varibles==============
+variable "d2list" {
+    default = ["apple", "banana", 3, 4]
+  
+}
+
+#access list variable in resources
+resource "null_resource" "NoResourceMap" {
+    
+    count = length(var.d2list)
+}
+
+#==============map varibles==============
+variable "d3map" {
+    default = {
+        fruit = {
+            name = "apple"
+            count = 3
+        }
+        vegitable ={
+            name = "carrot"
+            count = 5
+        }
+    }
+  
+}
+
+#access in resources
+resource "null_resource" "mapvaraible" {
+
+    #for each to be use
+    for_each = var.d3map
+
+}
