b85d38a7843088dc49e9f77d658d311888d8dec6
subbum <subbu@gmail.com>
2023-05-18 22:59:30 +0100

    subnet updated

--- roboshop-setup/ex4-yt.tf
+++ roboshop-setup/ex4-yt.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "MySubnet"
