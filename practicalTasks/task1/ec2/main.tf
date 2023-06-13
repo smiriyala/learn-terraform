8be0b5786b58bcb99238697520223c655a8ea879
subbum <subbu@gmail.com>
2023-06-13 22:46:00 +0100

    secgroup to list updated

--- practicalTasks/task1/ec2/main.tf
+++ practicalTasks/task1/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_spot_instance_request" "samplespotec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_sec_group_ids
+  vpc_security_group_ids = [ var.vpc_sec_group_ids ]
 
   tags = {
     Name = "UnderstandVariableModulez"
