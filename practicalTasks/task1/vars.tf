d57367d490b8e8efbe5513db227dba3dbd6ed144
subbum <subbu@gmail.com>
2023-06-13 18:06:03 +0100

    updated

--- practicalTasks/task1/vars.tf
+++ practicalTasks/task1/vars.tf
@@ -1,8 +1,8 @@
 #sggroup variable
 variable vpc_cidr {}
 
-#ec2 module varaibles
+/* #ec2 module varaibles
 variable ami_id{}
 variable "instance_type" {}
 
-variable "sggroupid" {}
+variable "sggroupid" {} */
