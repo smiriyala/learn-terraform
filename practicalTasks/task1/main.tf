88c62dfff4a29cdfe3f13c3833d573ece32f4647
subbum <subbu@gmail.com>
2023-06-13 22:40:19 +0100

    renamed variable for better understanding

--- practicalTasks/task1/main.tf
+++ practicalTasks/task1/main.tf
@@ -9,7 +9,7 @@ module "Ec2Instance_Creation" {
   ami_id = var.ami_id
   instance_type = var.instance_type
   #above module reference need to called OUTPUT variable
-  vpc_security_group_ids = module.SecGroupCreation.secgroup_out
+  vpc_sec_group_ids = module.SecGroupCreation.secgroup_out
   #sggroupid = module.sggroup.sggroupid
 
 }
