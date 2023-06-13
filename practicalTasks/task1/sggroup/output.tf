cc2195877c422bd29f074beab185a090eb7a9e20
subbum <subbu@gmail.com>
2023-06-13 22:31:14 +0100

    module relation output variable

---
+++ practicalTasks/task1/sggroup/output.tf
@@ -0,0 +1,3 @@
+output "secgroup_out" {
+    value = aws_security_group.allow_tls.id
+}
--- practicalTasks/task1/vars.tf
+++ practicalTasks/task1/vars.tf
@@ -1,8 +1,7 @@
 #sggroup variable
 variable vpc_cidr {}
 
-/* #ec2 module varaibles
+#ec2 module varaibles
 variable ami_id{}
 variable "instance_type" {}
 
-variable "sggroupid" {} */
